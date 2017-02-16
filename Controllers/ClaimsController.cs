e9818f171ca45d9b4fcd98f00882995cf0c13605
Jiatong Li <Jiatong Li>
2017-02-16 16:46:06 -0500

    Lab_four

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -1,33 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
+using FisherInsuranceApi.Data;
+using FisherInsuranceApi.Models;
 
 namespace FisherInsuranceApi.Controllers
 {
     [Route("api/claims")]
     public class ClaimsController : Controller
     {
-        // POST api/auto/quotes
         [HttpPost]
-        public IActionResult Post([FromBody]string value)
+        public IActionResult Post([FromBody]Claim claim)
         {
-            return Created("", value);
+            return Ok(db.CreateClaim(claim));
         }
-        // GET api/auto/quotes/5
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            return Ok("The id is: " + id);
+            return Ok(db.RetrieveQuote(id));
         }
-        // PUT api/auto/quotes/id
         [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody]string value)
+        public IActionResult Put([FromBody]Claim claim)
         {
-            return NoContent();
+            return Ok(db.UpdateClaim(claim));
         }
-        // DELETE api/auto/quotes/id
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            return Delete(id);
+            db.DeleteClaim(id);
+            return Ok();
         }
+        private IMemoryStore db;
+
+        public ClaimsController(IMemoryStore repo)
+        {
+            db = repo;
+            
+        }
+        [HttpGet]
+        public IActionResult GetClaims()
+        {
+        return Ok(db.RetrieveAllClaims);
+        }
+
     }
 }
