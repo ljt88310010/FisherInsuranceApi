65a90293fbaa8b8dd07776bc7008f772a45f734b
Nick Li <ljt88310010@gmail.com>
2017-04-11 17:56:20 -0400

    lab-seven

--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using FisherInsuranceApi.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using FisherInsuranceApi.Security;
 
 namespace FisherInsuranceApi
 {
@@ -30,20 +33,47 @@ namespace FisherInsuranceApi
         {
             services.AddDbContext<FisherContext>();
             // Add framework services.
+            services.AddIdentity<ApplicationUser, IdentityRole>(config =>
+            {
+            config.User.RequireUniqueEmail = true;
+            config.Password.RequireNonAlphanumeric = false;
+            config.Cookies.ApplicationCookie.AutomaticChallenge = false;
+            })
+            .AddEntityFrameworkStores<FisherContext>()
+            .AddDefaultTokenProviders();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseMvc();
-            
-            
-
-        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory
+loggerFactory)
+{
+loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+loggerFactory.AddDebug();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+//add our JwtProvider that we created
+app.UseJwtProvider();
+//add the built in authentication
+app.UseJwtBearerAuthentication(new JwtBearerOptions()
+{
+AutomaticAuthenticate = true,
+AutomaticChallenge = true,
+RequireHttpsMetadata = false,
+TokenValidationParameters = new TokenValidationParameters()
+{
+IssuerSigningKey = JwtProvider.SecurityKey,
+ValidIssuer = JwtProvider.Issuer,
+ValidateIssuerSigningKey = true,
+ValidateIssuer = false,
+ValidateAudience = false
+}
+});
+//if we wanted to support cookies, we do this:
+//app.UseIdentity();
+//or this
+//app.UseCookieAuthentication();
+app.UseMvc();
+}
     }
 }
--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -1,22 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import 'rxjs/Rx';
 // import our application component
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.components';
 import { NavBarComponent } from './components/navbar/navbar.components'; 
+import { LoginComponent } from './components/login/login.component';
+import { ClaimsComponent } from './components/claims/claims.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+import { AppRouting } from './app.routing';
+import { AuthService } from "./auth.service";
+import { AuthHttp } from "./auth.http";
+
 @NgModule({
  declarations: [
  AppComponent,
  HomeComponent,
- NavBarComponent
+ NavBarComponent,
+ LoginComponent,
+ ClaimsComponent,
+ QuotesComponent
  ],
  imports: [
  BrowserModule,
- HttpModule
+ HttpModule,
+ FormsModule,
+ ReactiveFormsModule,
+ RouterModule,
+ AppRouting
  ],
  providers: [ 
+    AuthService,
+    AuthHttp
  ],
  bootstrap: [
  AppComponent
